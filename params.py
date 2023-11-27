1bd787072c5bb3d1e7dc5cabcc386807db9f20f5
Lizzy den Dulk <dendulk.elizabeth@gmail.com>
2023-11-27 16:08:36 +0100

    adding preprocessing and multinomial

---
+++ params.py
@@ -0,0 +1,6 @@
+import os
+
+##################  CONSTANTS  #####################
+LOCAL_RAW_DATA_PATH = os.path.join(os.path.expanduser('~'), "detecting-sexism", "data", "raw_data")
+LOCAL_PROCESSED_DATA_PATH = os.path.join(os.path.expanduser('~'), "detecting-sexism", "data", "processed_data")
+LOCAL_REGISTRY_PATH =  os.path.join(os.path.expanduser('~'), ".lewagon", "mlops", "training_outputs")
