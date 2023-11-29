391ac972f5745a616e84f3db5c22ae8bd1961b10
Lizzy den Dulk <dendulk.elizabeth@gmail.com>
2023-11-29 13:52:27 +0100

    added train eval package

---
+++ app/packages/traineval.py
@@ -0,0 +1,72 @@
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras import models, metrics, Model
+import numpy as np
+
+
+def train_model(
+        model: Model,
+        X: np.ndarray,
+        y: np.ndarray,
+        batch_size=64,
+        patience=4,
+        validation_data=None, # overrides validation_split
+        validation_split=0.2
+    ) -> Tuple[Model, dict]:
+    """
+    Fit the model and return a tuple (fitted_model, history)
+    """
+    print("\nTraining model...")
+
+    es = EarlyStopping(
+        patience=patience,
+        restore_best_weights=True,
+        verbose=1
+    )
+
+    history = model.fit(
+        X,
+        y,
+        validation_data=validation_data,
+        validation_split=validation_split,
+        epochs=100,
+        batch_size=batch_size,
+        callbacks=[es],
+        verbose=0
+    )
+
+    print(f"✅ Model trained on {len(X)} rows with max val accuracy: {round(np.min(history.history['val_accuracy']), 2)}, max val recall: {round(np.min(history.history['val_recall']), 2)}")
+
+    return model, history
+
+def evaluate_model(
+        model: Model,
+        X: np.ndarray,
+        y: np.ndarray,
+        batch_size=64
+    ) -> Tuple[Model, dict]:
+    """
+    Evaluate trained model performance on the dataset
+    """
+
+    print(f"\nEvaluating model on {len(X)} rows...")
+
+    if model is None:
+        print(f"\n❌ No model to evaluate")
+        return None
+
+    metrics = model.evaluate(
+        x=X,
+        y=y,
+        batch_size=batch_size,
+        verbose=0,
+        # callbacks=None,
+        return_dict=True
+    )
+
+    loss = metrics["loss"]
+    accuracy = metrics["accuracy"]
+    recall = metrics["recall"]
+
+    print(f"✅ Model evaluated, recall: {round(recall, 2)}, accuracy: {round(accuracy, 2)}")
+
+    return metrics
