1f15fafcdcab6434bdc01ddcd4c1af23341e5501
Arnaud Blanchard <arnaud.v.blanchard@gmail.com>
2023-12-01 18:00:51 +0100

    workflow in progress, finishing pred, need a preproc pred

--- app/interface/workflow.py
+++ app/interface/workflow.py
@@ -10,47 +10,34 @@ from app.packages.preprocessing.preprocessing_ML import *
 from app.packages.data_storage.registry import *
 from params import *
 
-
-###### TO UPDATE COPY PASTA FROM TAXIFARE ######
-
-# @task
-# def preprocess_new_data(new_data: pd.DataFrame, model_name:str, preproc_params:dict):
-#     X_preproc, to_ignore = preproc_test(new_data,new_data, model_name, preproc_params)
-#     return X_preproc
+@task
+def clean_data(data:pd.DataFrame=None):
+    if data == None:
+        data = init_data(DB_URL)
+    cleaned_df = clean_new(data)
+    return cleaned_df
 
 @task
-def clean_data():
-    return clean_from_path()
-
-@task
-def preprocess_new_data(new_data: pd.DataFrame, model_name:str, preproc_params:dict):
-    X_preproc, to_ignore = preproc_test(new_data,new_data, model_name, preproc_params)
-    return X_preproc
+def preprocess_new_data(cleaned_df: pd.DataFrame, model_name:str, preproc_params:dict):
+    X_train_preproc, X_test_preproc, y_train, y_test = preprocess(model_name=model_name,cleaned_df=cleaned_df ,preproc_params=preproc_params)
+    return X_train_preproc, X_test_preproc, y_train, y_test
 
 
 @task
-def evaluate_production_model(min_date: str, max_date: str):
-    return evaluate(min_date,max_date)
+def evaluate_production_model(model_name:str,X_test_preproc, y_test, preproc_params:dict,stage:str="Production",batch_size:int=32):
+    return evaluate(model_name, X_test_preproc, y_test, preproc_params, stage, batch_size)
 
 @task
-def train():
-    pass
+def new_train(model_name, X_train_preproc, y_train, preproc_params,model_params):
+    return train(model_name, X_train_preproc, y_train, preproc_params,model_params)
 
-
-
-
-
-# @task
-# def re_train(min_date: str, max_date: str, split_ratio: str, lr:float ,b:int,pat:int):
-#     return train(min_date,max_date,split_ratio, lr,b, pat)
-
-# @task
-# def transition_model(current_stage: str, new_stage: str):
-#     return mlflow_transition_model(current_stage,new_stage)
+@task
+def transition_model(current_stage: str, new_stage: str):
+    return mlflow_transition_model(current_stage,new_stage)
 
 
 @flow(name=PREFECT_FLOW_NAME)
-def train_flow():
+def train_flow(model_name:str, preproc_params:dict, model_params:dict):
     """
     Build the Prefect workflow for the `taxifare` package. It should:
         - preprocess 1 month of new data, starting from EVALUATION_START_DATE
@@ -61,38 +48,45 @@ def train_flow():
     """
 
 
+    cleaned_df = clean_data()
+    X_train_preproc, X_test_preproc, y_train, y_test = preprocess_new_data(cleaned_df, model_name, preproc_params)
+    evaluate_production_model(model_name, X_test_preproc, y_test, preproc_params)
+
+    new_train(model_name, X_train_preproc, y_train, preproc_params,model_params)
+
+
     # Define your tasks
     # data = preprocess_new_data.submit(min_date, max_date)
     # old_mae = evaluate_production_model.submit(min_date, max_date,wait_for=[data])
 
-    batch_size = [128,256,512]#[64,128,256]
-    learning_rate = [0.1, 0.09, 0.05] #[0.1, 0.01, 0.005, 0.001]
-    patience = [2,4]#[2,4,6]
+    # batch_size = [128,256,512]#[64,128,256]
+    # learning_rate = [0.1, 0.09, 0.05] #[0.1, 0.01, 0.005, 0.001]
+    # patience = [2,4]#[2,4,6]
 
 
-    l = []
-    for b in batch_size:
-        for lr in learning_rate:
-            for pat in patience:
-                dico = {}
-                new_mae = re_train.submit(min_date, max_date, 0.2,lr,b,pat, wait_for=[data])
-                dico["new_mae"] = new_mae
-                dico["params"] = f"{lr},{b},{pat}"
-                l.append(dico)
+    # l = []
+    # for b in batch_size:
+    #     for lr in learning_rate:
+    #         for pat in patience:
+    #             dico = {}
+    #             new_mae = re_train.submit(min_date, max_date, 0.2,lr,b,pat, wait_for=[data])
+    #             dico["new_mae"] = new_mae
+    #             dico["params"] = f"{lr},{b},{pat}"
+    #             l.append(dico)
     # Compute your results as actual python object
-    l2 = []
-    old_mae_result = old_mae.result()
+    # l2 = []
+    # old_mae_result = old_mae.result()
 
-    for i in range(0,len(l)):
-        l2.append(l[i]["new_mae"].result())
+    # for i in range(0,len(l)):
+    #     l2.append(l[i]["new_mae"].result())
 
 
-    ind = l2.index(min(l2))
-    lr , b , p = l[ind]["params"]
+    # ind = l2.index(min(l2))
+    # lr , b , p = l[ind]["params"]
 
-    print(f"Best Params -  learning_rate: {lr} / batch_size: {b} / patience: {p}")
-    new_mae = re_train.submit(min_date, max_date, 0.2,lr,b,p)
-    new_mae_result = new_mae.result()
+    # print(f"Best Params -  learning_rate: {lr} / batch_size: {b} / patience: {p}")
+    # new_mae = re_train.submit(min_date, max_date, 0.2,lr,b,p)
+    # new_mae_result = new_mae.result()
 
     # new_mae_result
 
--- app/packages/preprocessing/preprocessing_ML.py
+++ app/packages/preprocessing/preprocessing_ML.py
@@ -7,69 +7,13 @@ from keras.preprocessing.text import text_to_word_sequence, Tokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from keras.utils import pad_sequences
 import numpy as np
-from gensim.models import Word2Vec
-
-@simple_time_and_memory_tracker
-def tokenize_words(data:pd.DataFrame, text_col:str): #tokenizes text to words
-    df = data.copy()
-    df[text_col] = df[text_col].apply(lambda x: word_tokenize(x))
-    return df
-
-@simple_time_and_memory_tracker
-def tokenize_sentences(data:pd.DataFrame, text_col:str): #tokenizes text to sentences
-    df = data.copy()
-    df[text_col] = df[text_col].apply(lambda x: sent_tokenize(x))
-    return df
-
-@simple_time_and_memory_tracker
-def remove_stop_words(data:pd.DataFrame, text_col:str): # remove stop words (english)
-    df = data.copy()
-    stop_words = set(stopwords.words('english')) # Make stopword list
-    df[text_col] = df[text_col].apply(lambda x: [word for word in x if not word in stop_words])
-    return df
-
-@simple_time_and_memory_tracker
-def lemmatizer(data:pd.DataFrame, text_col:str): # lemmatize text
-    df = data.copy()
-    lem = WordNetLemmatizer()
-    df[text_col] = df[text_col].apply(lambda x: " ".join([lem.lemmatize(word, pos="v")for word in x]))
-    return df
-
-
-##############################################################################################################
-###################### MAIN PREPROCESSING FUNCTION ##########################################################
-###############################################################################################################
-
-
-def preprocessing(data:pd.DataFrame, text_col:str, stop_words=True, sent_tokenize=False, lemmatize=False):
-
-    if sent_tokenize==True:             ### tokenize rows to words or sentences
-        tokenized = tokenize_sentences(data,text_col)
-    else:
-        tokenized = tokenize_words(data,text_col)
-        # tokenized = [word for word in tokenized if word.isalpha()] # Remove numbers
-
-    if stop_words==True:                ### remove stop words (if stop_words==True)
-        tokenized = remove_stop_words(tokenized, text_col)
-
-    if lemmatize==True:                 ### lemmatize sentences
-        tokenized = lemmatizer(tokenized, text_col)
-
-    print("✅ Preprocessing is done")
-    return tokenized
-
-
-
-##############################   UPDATING PREPROCESSING  ################################################
-
+from gensim.models import Word2Vec, KeyedVectors
 
 
 model_names = ["conv1d", "GRU", "LSTM", "multinomial", "BERT"]
 
 
 def preproc_test(X_train:pd.DataFrame,X_test:pd.DataFrame, model_name:str, params : dict=None):
-
-
     def LSTM_preprocess(X_train, X_test):
         max_length = 100
         vector_size=50
@@ -94,7 +38,7 @@ def preproc_test(X_train:pd.DataFrame,X_test:pd.DataFrame, model_name:str, param
             Takes padding arguments dtype & padding
             """
             word2vec = Word2Vec(sentences=X_train, vector_size=vector_size, window=window)
-
+            word2vec.save(f"./training_outputs/W2V/{params['max_length']}_{vector_size}_{window}_{padding}_W2V.wordvectors")
             def embed_sentence(wv, sentence):
                 return np.array([wv[i] for i in sentence if i in wv])
             wv = word2vec.wv
@@ -213,6 +157,132 @@ def preproc_test(X_train:pd.DataFrame,X_test:pd.DataFrame, model_name:str, param
         return GRU_preprocess(X_train, X_test)
     if model_name == "conv1d":
         return Conv1d_preprocess(X_train, X_test)
+
+    if model_name == "BERT":
+        BERT_preprocess()
+
+def preproc_pred(X_pred:pd.DataFrame,model_name:str, params : dict=None):
+    def LSTM_preprocess(X_pred, ):
+        max_length = 100
+        vector_size=50
+        window=5
+
+        def tokenize(df_column, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=' '):
+            """ tokenize a column
+            df_column (pandas.Series): The DataFrame column containing text.
+            filters: The set of characters to filter out. defaults to remove punctuation.
+            lower: Whether to convert the text to lowercase. defaults to true.
+            split: The split to use for splitting the text. Defaults to ' ' (space).
+
+            Returns:
+            list of lists: list where each element is a list of tokens from a row in the input column.
+            """
+            return df_column.astype(str).apply(lambda x: text_to_word_sequence(x, filters=filters, lower=lower, split=split)).tolist()
+
+
+        def w2v_embed(X_pred, max_length, dtype='float32', padding='post'):
+            """
+            Embed sentences using a trained Word2Vec model.
+            """
+
+            word2vec_model = KeyedVectors.load("word2vec.wordvectors", mmap='r')
+            def embed_sentence(wv, sentence):
+                return np.array([wv[i] for i in sentence if i in wv])
+            # Embedding the sentences
+            wv = word2vec_model.wv
+            embedded_X = [embed_sentence(wv, s) for s in X_pred]
+            return pad_sequences(embedded_X, maxlen=max_length, dtype=dtype, padding=padding)
+
+        X_pred_token = tokenize(X_pred.text)
+        X_pred_truncated = [sentence[:max_length] for sentence in X_pred_token]
+        X_pred_padded = w2v_embed(X_pred_truncated, vector_size=vector_size, window=window)
+        print(f"Shape of X_pred_padded in preproc: {X_pred_padded.shape}")
+        return X_pred_padded
+
+    def Embed_LSTM_preproc(X_pred:pd.DataFrame, params:dict):
+        tk = Tokenizer()
+        tk.fit_on_texts(X_pred.text)
+        vocab_size = len(tk.word_index)
+        print(f'There are {vocab_size} different words in your corpus')
+        X_pred_token = tk.texts_to_sequences(X_pred.text)
+        X_pred_truncated = [sentence[:params["max_length"]] for sentence in X_pred_token]
+        X_pred_pad = pad_sequences(X_pred_truncated, maxlen=params["max_length"])
+        return X_pred_pad
+    def Multinomial_preprocess(X_pred, ):
+
+        @simple_time_and_memory_tracker
+        def tokenize_words(X_pred): #tokenizes text to words
+            X_pred = X_pred.apply(lambda x: word_tokenize(x))
+            return X_pred
+
+        @simple_time_and_memory_tracker
+        def tokenize_sentences(X_pred): #tokenizes text to sentences
+            X_pred = X_pred.apply(lambda x: sent_tokenize(x))
+            return X_pred,
+
+        @simple_time_and_memory_tracker
+        def remove_stop_words(X_pred): # remove stop words (english)
+            stop_words = set(stopwords.words('english')) # Make stopword list
+            X_pred = X_pred.apply(lambda x: [word for word in x if not word in stop_words])
+            return X_pred,
+
+        @simple_time_and_memory_tracker
+        def lemmatizer(X_pred): # lemmatize text
+            lem = WordNetLemmatizer()
+            X_pred = X_pred.apply(lambda x: " ".join([lem.lemmatize(word, pos="v")for word in x]))
+            return X_pred
+
+        def preprocessing(X_pred, stop_words=True, sent_tokenize=False, lemmatize=False):
+            if sent_tokenize==True:             ### tokenize rows to words or sentences
+                X_pred_token = tokenize_sentences(X_pred)
+            else:
+                X_pred_token = tokenize_words(X_pred)
+            if stop_words==True:                ### remove stop words (if stop_words==True)
+                X_pred_token = remove_stop_words(X_pred_token)
+            if lemmatize==True:                 ### lemmatize sentences
+                X_pred_token = lemmatizer(X_pred_token)
+            print("✅ Preprocessing is done")
+            return X_pred_token
+        return preprocessing(X_pred,stop_words=False, sent_tokenize=False, lemmatize=True)
+    def GRU_preprocess(X_pred, ):
+        return LSTM_preprocess(X_pred, )
+
+    def Conv1d_preprocess(X_pred):
+        max_length = 100
+        @simple_time_and_memory_tracker
+        def preprocessing_cld(X : pd.DataFrame,maxlen=max_length):
+            """ Preprocess X data for a Conv1D model
+            Takes a single column df, X (as a list), as input. Returns the preprocessed X,
+            the maxlen and the vocab size as output for use in initialize model function
+            """
+            X_l = X.tolist()
+            X_word = [text_to_word_sequence(x) for x in X_l]
+
+            tk = Tokenizer()
+            tk.fit_on_texts(X_word)
+            X_token = tk.texts_to_sequences(X_word)
+            vocab_size = len(tk.word_index)
+
+            X_token_pad = pad_sequences(X_token, dtype=float, padding='post', maxlen=max_length)
+            return X_token_pad, vocab_size, max_length
+        X_pred_pad, train_vocab_size, train_max_length = preprocessing_cld(X_pred)
+        train = []
+        return train.append([X_pred_pad, train_vocab_size, train_max_length])
+
+    def BERT_preprocess():
+        pass
+
+    if model_name == "LSTM":
+        if params["embed"] == True:
+            return Embed_LSTM_preproc(X_pred, params)
+        else:
+            return LSTM_preprocess(X_pred)
+    if model_name == "multinomial":
+        return Multinomial_preprocess(X_pred)
+    if model_name == "GRU":
+        return GRU_preprocess(X_pred)
+    if model_name == "conv1d":
+        return Conv1d_preprocess(X_pred)
 
     if model_name == "BERT":
         BERT_preprocess()
